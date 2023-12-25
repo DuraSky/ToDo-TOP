2727e00fe9e46d90752e8605a75359a2cda513f8
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-12-25 11:16:46 +0100

    creating nested arrays now

--- src/index.js
+++ src/index.js
@@ -8,31 +8,24 @@ let allProjects = [];
 class Project {
     constructor(projectName){
         this.projectName = projectName;
+        this.task = {}
+    }
+
+    addTask(newTask){
+        this.task = newTask;
     }
 }
 
-// class addProperties extends Project{
-//     constructor
-// }
+class Task {
+    constructor(priority){
+        this.priority = priority;
+    }
+}
+
 
 subButton.addEventListener("click", ()=>{
     const projectNameDiv = document.createElement("div");
     projectNameDiv.setAttribute("id", "projectNameDiv");
-
-    const addProperty = document.createElement("input");
-    addProperty.setAttribute("type", "text");
-    addProperty.setAttribute("name", "setProperty");
-    addProperty.setAttribute("id", "setProperty");
-    const addLabel = document.createElement("label");
-    addLabel.setAttribute("for", "setProperty");
-    addLabel.innerHTML = "Add Property";
-    const addButton = document.createElement("button");
-    addButton.innerHTML = "add"
-
-    addButton.addEventListener("click", ()=>{
-        const propertyValue = addProperty.value;
-        return propertyValue;
-    })
 
     const theNameIs = projectNameForm.value;
     console.log(theNameIs);
@@ -40,15 +33,34 @@ subButton.addEventListener("click", ()=>{
     allProjects.push(createNewProject);
     projectNameDiv.innerHTML = theNameIs;
 
+    const addTask = document.createElement("input");
+    addTask.setAttribute("type", "text");
+    addTask.setAttribute("name", "setProperty");
+    addTask.setAttribute("id", "setProperty");
+    const addLabel = document.createElement("label");
+    addLabel.setAttribute("for", "setProperty");
+    addLabel.innerHTML = "Add Task";
+    const addButton = document.createElement("button");
+    addButton.innerHTML = "add"
+
+     addButton.addEventListener("click", ()=>{
+         const taskValue = addTask.value;
+         console.log(taskValue)
+
+        let createTask = new Task(taskValue);
+        createNewProject.addTask(createTask)
+         console.log(...allProjects)
+     })
+
     content.appendChild(projectNameDiv);
     projectNameDiv.appendChild(addLabel);
-    projectNameDiv.appendChild(addProperty);
+    projectNameDiv.appendChild(addTask);
     projectNameDiv.appendChild(addButton);
     
 
     projectNameForm.value = "";
-    // console.log(createNewProject);
-    console.log(...allProjects);
+    console.log(createNewProject);
+
 });
 
 //console.log(propertyValue)
